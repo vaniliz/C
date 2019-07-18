7f60c72547538118c2e811136901f9a88cf35cc1
vaniliz <vaniliz.14@gmail.com>
2019-07-18 15:57:50 -0500

    tercer video

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 
 int main()
 {
- printf("User is awesome\n")/*\t es para tab, \a es para alerta, \n para salto de linea*/
- printf("User is cool");
+ printf("%s is the best person ever\n", "This User");/* %s es sustituido por strings*/
+ printf("%s is the best %s ever\n","This User","programmer" );
+ printf("I ate %d corndog last night\n", 9);/* %d es sustituido por un numero entero*/
+ printf("El numero Pi vale %f\n", 3.14159);/* %f es sustituido por un numero decimal*/
+ printf("El numero Pi vale %.4f\n", 3.14159);
+ printf("El numero Pi vale %.2f\n", 3.14159);/*El .# despues del % indica el numero de decimales que se mostraran en pantalla*/
     return 0;
 }
