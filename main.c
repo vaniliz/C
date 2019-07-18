4f792853ec6ca3510c338614fbd89eee65be02b6
vaniliz <vaniliz.14@gmail.com>
2019-07-18 13:42:16 -0500

    segundo video

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ printf("User is awesome\n")/*\t es para tab, \a es para alerta, \n para salto de linea*/
+ printf("User is cool");
+    return 0;
+}
